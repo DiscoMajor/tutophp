4916a336443b4847b83f0619bf5d75a5913071d7
DiscoMajor <coolstorybrotherz@gmail.com>
2020-04-05 16:33:13 +0200

    I FIXED MY IMAGES

--- js/player.js
+++ js/player.js
@@ -5,3 +5,13 @@ document.getElementById("play").onclick = function() {
 document.getElementById("pause").onclick = function() {
     document.getElementById('demo').pause();
 }
+
+/* TRY PLAYER */
+
+document.getElementById("play").onclick = function() {
+    document.getElementById('demo').play();
+}
+
+document.getElementById("pause").onclick = function() {
+    document.getElementById('demo').pause();
+}
